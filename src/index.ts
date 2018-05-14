67a95f47bc772799669e63379a54deca8606a8b9
Zoltan Fodor <zoltan.fodor@pitechnologies.ro>
2018-05-14 12:59:06 +0300

    Added support for linked readme files.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as Path from 'path';
+import { Application } from './lib/application';
+import { ProjectReflection } from './lib/models/reflections';
+
 export { Application } from './lib/application';
 export { CliApplication } from './lib/cli';
 
@@ -10,3 +14,19 @@ export { Renderer } from './lib/output/renderer';
 export { DefaultTheme } from './lib/output/themes/DefaultTheme';
 export { NavigationItem } from './lib/output/models/NavigationItem';
 export { UrlMapping } from './lib/output/models/UrlMapping';
+
+const src = Path.join(__dirname, '..', 'examples', 'basic', 'src');
+const out = Path.join(__dirname, '..', 'tmp', 'test');
+let app: Application = new Application({
+        mode:   'Modules',
+        logger: 'console',
+        target: 'ES5',
+        module: 'CommonJS',
+        gaSite: 'foo.com' // verify theme option without modifying output
+    }),
+    project: ProjectReflection = new ProjectReflection('test');
+
+const input = app.expandInputFiles([src]);
+project = app.convert(input);
+
+app.generateDocs(project, out);
